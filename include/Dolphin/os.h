05a4603f2b6f07348127ab6a3766dddd410598dc
intns <84647527+intns@users.noreply.github.com>
2021-11-08 12:22:19 +0000

    Move Round define to types.h as it is universal, apply changes appropriately

--- include/Dolphin/os.h
+++ include/Dolphin/os.h
@@ -7,13 +7,10 @@
 extern "C" {
 #endif // ifdef __cplusplus
 
+// OS logging
 void OSReport(const char*, ...);
 void OSPanic(const char* file, int line, const char* message, ...);
 #define OSError(...) OSPanic(__FILE__, __LINE__, __VA_ARGS__)
-
-// Rounds up and down to multiples of 20
-#define RoundUpTo20(x)   (((u32)(x) + 0x1F) & ~(0x1F))
-#define RoundDownTo20(x) (((u32)(x)) & ~(0x1F))
 
 // TODO: fill these structs
 typedef struct OSContext {
